27ea3a2c59016fa911c9ec496559072020e85767
Harut Tosunyan <haruttosunyan@gmail.com>
2019-04-08 16:08:15 +0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HostListener} from "@angular/core";
+import { ViewChild, ElementRef } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'headertest';
+  smlIcon = false;
+  smlBtn = false;
+  @ViewChild('h1') h1: ElementRef;
+  @ViewChild('bo') bo: ElementRef;
+
+  constructor() {
+    this.onScroll();
+  }
+
+  @HostListener('window:scroll', []) // for window scroll events
+  onScroll() {
+    if (this.h1) {
+      this.smlIcon = window.scrollY > this.h1.nativeElement.offsetTop - 60;
+    }
+    if (this.bo) {
+      this.smlBtn = window.scrollY > this.bo.nativeElement.offsetTop - 60;
+    }
+  }
 }
